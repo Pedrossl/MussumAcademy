ae75bdc3f16a91c6c20ac23c80054fbaf730593f
pedro <pedrosslobato@gmail.com>
2023-09-25 20:12:35 -0300

    :hammer: added nestjsQuery conf to module

--- src/modules/lesson/lesson.module.ts
+++ src/modules/lesson/lesson.module.ts
@@ -1,6 +1,27 @@
+import { NestjsQueryGraphQLModule, PagingStrategies } from '@nestjs-query/query-graphql';
+import { NestjsQueryTypeOrmModule } from '@nestjs-query/query-typeorm';
 import { Module } from '@nestjs/common';
+import { Lesson } from './entities/lesson.entity';
+import { LessonDTO } from './dto/lesson.dto';
+import { CreateLessonDTO } from './dto/create-lesson.input';
 
 @Module({
+  imports: [
+    NestjsQueryGraphQLModule.forFeature({
+      imports: [
+        NestjsQueryTypeOrmModule.forFeature([Lesson]),
+      ],
+      resolvers: [
+        {
+          DTOClass: LessonDTO,
+          EntityClass: Lesson, 
+          CreateDTOClass: CreateLessonDTO,
+          enableTotalCount: true,
+          pagingStrategy: PagingStrategies.OFFSET
+        }
+  ],
+    }),
+  ],
   providers: [],
 })
 export class LessonModule {}
