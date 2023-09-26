36fcdb8ae3306be51c6f9737d63e8cffadcef854
pedro <pedrosslobato@gmail.com>
2023-09-25 21:22:27 -0300

    :adhesive_bandage: Fix updadeDTO

--- src/modules/diciplines/dto/update-dicipline.input.ts
+++ src/modules/diciplines/dto/update-dicipline.input.ts
@@ -1,8 +1,8 @@
-import { CreateDiciplineInput } from './create-dicipline.input';
+import { CreateDiciplineDTO} from './create-dicipline.input';
 import { InputType, Field, Int, PartialType } from '@nestjs/graphql';
 
 @InputType()
-export class UpdateDiciplineInput extends PartialType(CreateDiciplineInput) {
+export class UpdateDiciplineInput extends PartialType(CreateDiciplineDTO) {
   @Field(() => Int)
   id: number;
 }
