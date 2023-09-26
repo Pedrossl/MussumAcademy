a75cdd2bcde2bfd24bab01529dbd347088bccb13
pedro <pedrosslobato@gmail.com>
2023-09-26 07:53:34 -0300

    :wrench: FilterableField lesson-content

--- src/modules/content/dto/content.dto.ts
+++ src/modules/content/dto/content.dto.ts
@@ -1,8 +1,10 @@
-import { FilterableField } from "@nestjs-query/query-graphql";
+import { FilterableField, FilterableRelation } from "@nestjs-query/query-graphql";
 import { ObjectType } from "@nestjs/graphql";
 import { BaseDTO } from "src/modules/base/dto/base.dto";
+import { LessonDTO } from "src/modules/lesson/dto/lesson.dto";
 
 @ObjectType('Content')
+@FilterableRelation('lesson', () => LessonDTO)
 export class ContentDTO extends BaseDTO {
     @FilterableField()
     description: string;
