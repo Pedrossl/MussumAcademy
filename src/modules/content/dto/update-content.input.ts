bd8f93cbec0128016258ee8d3230574213e34a6c
pedro <pedrosslobato@gmail.com>
2023-09-25 22:06:14 -0300

    :recycle: alter name to DTO

--- src/modules/content/dto/update-content.input.ts
+++ src/modules/content/dto/update-content.input.ts
@@ -2,7 +2,7 @@ import { CreateContentDTO } from './create-content.input';
 import { InputType, Field, Int, PartialType } from '@nestjs/graphql';
 
 @InputType()
-export class UpdateContentInput extends PartialType(CreateContentDTO) {
+export class UpdateContentDTO extends PartialType(CreateContentDTO) {
   @Field(() => Int)
   id: number;
 }
