b31efa58a2ee27b9fb0436f87e129ec0368e38db
pedro <pedrosslobato@gmail.com>
2023-09-26 09:06:02 -0300

    :hammer: added offseconection with dicipline

--- src/modules/lesson/dto/lesson.dto.ts
+++ src/modules/lesson/dto/lesson.dto.ts
@@ -2,8 +2,10 @@ import { FilterableField, FilterableOffsetConnection } from "@nestjs-query/query
 import { ObjectType } from "@nestjs/graphql";
 import { BaseDTO } from "src/modules/base/dto/base.dto";
 import { ContentDTO } from "src/modules/content/dto/content.dto";
+import { DiciplineDTO } from "src/modules/diciplines/dto/dicipline.dto";
 
 @ObjectType('lesson')
+@FilterableOffsetConnection('dicipline',() => DiciplineDTO,{nullable: true})
 @FilterableOffsetConnection('contents',() => ContentDTO,{nullable: true})
 export class LessonDTO extends BaseDTO{
     
