9a39e0c0cc9a20a7206e04bce07a0e9108c321fc
pedro <pedrosslobato@gmail.com>
2023-09-26 07:58:23 -0300

    :hammer: added OneToMany Content-Lesson

--- src/modules/lesson/dto/lesson.dto.ts
+++ src/modules/lesson/dto/lesson.dto.ts
@@ -1,13 +1,13 @@
-import { FilterableField } from "@nestjs-query/query-graphql";
+import { FilterableField, FilterableOffsetConnection } from "@nestjs-query/query-graphql";
 import { ObjectType } from "@nestjs/graphql";
 import { BaseDTO } from "src/modules/base/dto/base.dto";
-
-
+import { ContentDTO } from "src/modules/content/dto/content.dto";
 
 @ObjectType('lesson')
+@FilterableOffsetConnection('contents',() => ContentDTO,{nullable: true})
 export class LessonDTO extends BaseDTO{
     
     @FilterableField()
     description: string;
 
-    } 
+} 
--- src/modules/lesson/entities/lesson.entity.ts
+++ src/modules/lesson/entities/lesson.entity.ts
@@ -1,8 +1,13 @@
-import { Column, Entity } from "typeorm";
+import { Column, Entity, OneToMany } from "typeorm";
 import { Base } from "../../base/entities/base.entity";
+import { Content } from "src/modules/content/entities/content.entity";
 
 @Entity()
 export class Lesson extends Base {
-@Column()
-description: string;
+    @Column()
+    description: string;
+
+
+    @OneToMany(() => Content, (content) => content.lesson)
+    contents: Content[];
 }
