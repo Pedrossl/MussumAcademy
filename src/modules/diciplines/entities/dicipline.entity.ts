4be59002a769cb13f94c7fa36e4960fe95c98e42
pedro <pedrosslobato@gmail.com>
2023-09-26 09:05:26 -0300

    :hammer: added added student relationships and lessons

--- src/modules/diciplines/entities/dicipline.entity.ts
+++ src/modules/diciplines/entities/dicipline.entity.ts
@@ -1,5 +1,7 @@
 import { Base } from 'src/modules/base/entities/base.entity';
-import { Column, Entity } from 'typeorm';
+import { Lesson } from 'src/modules/lesson/entities/lesson.entity';
+import { Student } from 'src/modules/students/entities/student.entity';
+import { Column, Entity, JoinTable, ManyToMany, OneToMany } from 'typeorm';
 
 @Entity()
 export class Dicipline extends Base {
@@ -7,4 +9,12 @@ export class Dicipline extends Base {
   @Column()
   name: string;
 
+  @ManyToMany(() => Student, students => students.diciplines,{
+    nullable: true,
+  })
+  @JoinTable()
+  students: Student[];
+
+  @OneToMany(() => Lesson, (lesson) => lesson.dicipline)
+  lessons: Lesson[];
 }
