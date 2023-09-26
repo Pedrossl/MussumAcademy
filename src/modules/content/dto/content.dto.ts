bab725706cd0896233718a415bbfc4e651b7de98
pedro <pedrosslobato@gmail.com>
2023-09-25 21:37:31 -0300

    🔨 added contentDTO

---
+++ src/modules/content/dto/content.dto.ts
@@ -0,0 +1,12 @@
+import { FilterableField } from "@nestjs-query/query-graphql";
+import { ObjectType } from "@nestjs/graphql";
+import { BaseDTO } from "src/modules/base/dto/base.dto";
+
+@ObjectType('Content')
+export class ContentDTO extends BaseDTO {
+    @FilterableField()
+    description: string;
+  
+    @FilterableField()
+    linkContent: string;
+  }
