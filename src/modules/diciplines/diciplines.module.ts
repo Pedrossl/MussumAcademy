0d3bdb8345a22ee98d25c20d0fe52c7694657479
pedro <pedrosslobato@gmail.com>
2023-09-25 21:20:49 -0300

    🔨 added nestjsQuery conf to module

--- src/modules/diciplines/diciplines.module.ts
+++ src/modules/diciplines/diciplines.module.ts
@@ -1,8 +1,27 @@
+import { NestjsQueryGraphQLModule, PagingStrategies } from '@nestjs-query/query-graphql';
+import { NestjsQueryTypeOrmModule } from '@nestjs-query/query-typeorm';
 import { Module } from '@nestjs/common';
-import { DiciplinesService } from './diciplines.service';
-import { DiciplinesResolver } from './diciplines.resolver';
+import { Dicipline } from './entities/dicipline.entity';
+import { CreateDiciplineDTO } from './dto/create-dicipline.input';
+import { DiciplineDTO } from './dto/dicipline.dto';
 
 @Module({
-  providers: [DiciplinesResolver, DiciplinesService],
+  imports: [
+    NestjsQueryGraphQLModule.forFeature({
+      imports: [
+        NestjsQueryTypeOrmModule.forFeature([Dicipline]),
+      ],
+      resolvers: [
+        {
+          DTOClass: DiciplineDTO,
+          EntityClass: Dicipline, 
+          CreateDTOClass: CreateDiciplineDTO,
+          enableTotalCount: true,
+          pagingStrategy: PagingStrategies.OFFSET
+        }
+  ],
+    }),
+  ],
+  providers: [],
 })
 export class DiciplinesModule {}
